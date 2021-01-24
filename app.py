7c2900208503ec2fd6ba04a3134583b3260e4719
Luca Marino <marino.luca07@gmail.com>
2021-01-24 19:44:44 +0100

    main

--- app.py
+++ app.py
@@ -1,16 +1,22 @@
 import streamlit as st
 from genetico import run_genetico
-from test_chess import Checkerboard
+from test_chess import chess_board
 import matplotlib.pyplot as plt
+import numpy as np
+
+st.set_option('deprecation.showPyplotGlobalUse', False)
 
 regine = st.number_input('REGINE', min_value=4 ,step=1)
 
-chess_board = Checkerboard(regine**2, regine)
 
 
 a =st.button('RUN')
 
 if a is True:
-    st.pyplot(plt.imshow(chess_board, cmap='gray'))
+
     soluzione = run_genetico(regine, 100)
     st.text(soluzione)
+    plt.imshow(chess_board(regine=regine), cmap='binary')
+    plt.axis('off')
+    plt.scatter(np.array(soluzione[0])-1, range(0,regine))
+    st.pyplot()
--- test_chess.py
+++ test_chess.py
@@ -6,18 +6,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-def Checkerboard(N,n):
-    """N: size of board; n=size of each square; N/(2*n) must be an integer """
-    if (N%(2*n)):
-        print('Error: N/(2*n) must be an integer')
-        return False
-    a = np.concatenate((np.zeros(n),np.ones(n)))
-    b=np.pad(a,int((N**2)/2-n),'wrap').reshape((N,N))
-    x = (b+b.T==1).astype(int)
-    return np.where(x == 0, 1, 0)
+def chess_board(regine):
+    chessboard = np.zeros((regine,regine))
+    chessboard[1::2, 0::2] = 1
+    chessboard[0::2, 1::2] = 1
 
-# B=Checkerboard(64,8)
-# print(B[0].T)
-# df = pd.DataFrame(B)
-# plt.imshow(B)
-# plt.show()
+    return chessboard
