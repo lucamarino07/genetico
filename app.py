ab22a14e47f41f489cca46b4c5da1517165a2d4f
Luca Marino <marino.luca07@gmail.com>
2021-01-24 19:14:39 +0100

    main

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 import streamlit as st
 from genetico import run_genetico
-
+from test_chess import Checkerboard
+import matplotlib.pyplot as plt
 
 regine = st.number_input('REGINE', min_value=4 ,step=1)
 
+chess_board = Checkerboard(regine**2, regine)
 
 
 a =st.button('RUN')
 
 if a is True:
+    st.pyplot(plt.imshow(chess_board, cmap='gray'))
     soluzione = run_genetico(regine, 100)
     st.text(soluzione)
--- test_chess.py
+++ test_chess.py
@@ -1,31 +1,23 @@
-from PIL import Image, ImageDraw
-from itertools import cycle
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 
-def draw_chessboard(n=8, pixel_width=500):
-    """
-    Draw an n x n chessboard using PIL.
-    """
-    def sq_start(i):
-        """
-        Return the square corners, suitable for use in PIL drawings
-        """
-        return i*pixel_width / n
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 
-    def square(i, j):
-        """
-        Return the square corners, suitable for use in PIL drawing
-        """
-        return map(sq_start, [i, j, i+1, j+1])
+def Checkerboard(N,n):
+    """N: size of board; n=size of each square; N/(2*n) must be an integer """
+    if (N%(2*n)):
+        print('Error: N/(2*n) must be an integer')
+        return False
+    a = np.concatenate((np.zeros(n),np.ones(n)))
+    b=np.pad(a,int((N**2)/2-n),'wrap').reshape((N,N))
+    x = (b+b.T==1).astype(int)
+    return np.where(B == 0, 1, 0)
 
-    image = Image.new("L", (pixel_width, pixel_width))
-    draw_square = ImageDraw.Draw(image).rectangle
-    squares = (square(i,j)
-              for i_start, j in zip(cycle((0, 1)), range(n))
-               for i in range(i_start, n, 2))
-
-    for sq in squares:
-        draw_square(sq, fill='white')
-        image.save("chessboard.png")
-
-# draw 8 x 8 chess board with 500 pixel as pixel width
-draw_chessboard(8)
+B=Checkerboard(64,8)
+print(B[0].T)
+df = pd.DataFrame(B)
+# plt.imshow(B)
+# plt.show()
