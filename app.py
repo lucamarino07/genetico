98373723e36c333b069f01ea106be7c543ee9a0a
Luca Marino <marino.luca07@gmail.com>
2021-01-24 20:08:55 +0100

    main

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from genetico import run_genetico
-from test_chess import chess_board
+from test_chess import chess_board, imscatter
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -9,14 +9,20 @@ st.set_option('deprecation.showPyplotGlobalUse', False)
 regine = st.number_input('REGINE', min_value=4 ,step=1)
 
 
-
+zoom = (regine/regine**2) * 0.4
 a =st.button('RUN')
 
 if a is True:
 
     soluzione = run_genetico(regine, 100)
-    st.text(soluzione)
+
+    x = np.array(soluzione[0])-1
+    y = range(0,regine)
+    image_path = 'queen.png'
+    imscatter(x, y, image_path, zoom=zoom)
     plt.imshow(chess_board(regine=regine), cmap='binary')
-    plt.axis('off')
-    plt.scatter(np.array(soluzione[0])-1, range(0,regine))
+    # plt.axis('off')
+    ax = plt.gca()
+    ax.axes.xaxis.set_visible(False)
+    ax.axes.yaxis.set_visible(False)
     st.pyplot()
--- test_chess.py
+++ test_chess.py
@@ -5,6 +5,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+from matplotlib.offsetbox import OffsetImage, AnnotationBbox
+
 
 def chess_board(regine):
     chessboard = np.zeros((regine,regine))
@@ -12,3 +14,21 @@ def chess_board(regine):
     chessboard[0::2, 1::2] = 1
 
     return chessboard
+
+def imscatter(x, y, image, ax=None, zoom=1):
+    if ax is None:
+        ax = plt.gca()
+    try:
+        image = plt.imread(image)
+    except TypeError:
+        # Likely already an array...
+        pass
+    im = OffsetImage(image, zoom=zoom)
+    x, y = np.atleast_1d(x, y)
+    artists = []
+    for x0, y0 in zip(x, y):
+        ab = AnnotationBbox(im, (x0, y0), xycoords='data', frameon=False)
+        artists.append(ax.add_artist(ab))
+    ax.update_datalim(np.column_stack([x, y]))
+    ax.autoscale()
+    return artists
