6476965feae625afd13a6efc51089fa1a9f6c073
Luca Marino <marino.luca07@gmail.com>
2021-01-28 15:28:36 +0100

    a

--- app.py
+++ app.py
@@ -3,12 +3,13 @@ from genetico import run_genetico
 from test_chess import chess_board, imscatter
 import matplotlib.pyplot as plt
 import numpy as np
+import time
 
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
 st.set_page_config(
         page_title='Problema delle regine',
-        layout='wide',
+        # layout='wide',
         initial_sidebar_state='auto',
         page_icon='♟',
     )
@@ -22,14 +23,23 @@ footer {visibility: hidden;}
 """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
 
+st.markdown("""<p>Il <b>problema delle regine</b> 👑 è un problema che consiste 
+nel trovare il modo di posizionare <i>N</i> donne (pezzo degli scacchi ♟) su 
+una scacchiera <i>N</i>x<i>N</i> tali che nessuna di esse possa catturarne un'altra, usando i 
+movimenti standard della regina. Perciò, una soluzione dovrà prevedere che 
+nessuna regina abbia una colonna, traversa o diagonale in comune con un'altra 
+regina.<br><br><b>Inserisci il numero di regine e scopri la soluzione</b></p>""", unsafe_allow_html=True)
+
+
 regine = st.number_input('REGINE', min_value=4 ,step=1)
 
 
 zoom = (regine/regine**2) * 0.4
-a =st.button('RUN')
+a =st.button('Play')
 
 if a is True:
 
+    start_time = time.time()
     soluzione = run_genetico(regine, 100)
 
     x = np.array(soluzione[0])-1
@@ -42,3 +52,4 @@ if a is True:
     ax.axes.xaxis.set_visible(False)
     ax.axes.yaxis.set_visible(False)
     st.pyplot()
+    st.write(time.time() - start_time)
