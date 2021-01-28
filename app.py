48dd8e3c82c577663dc9569c251d77f434e9c1b0
Luca Marino <marino.luca07@gmail.com>
2021-01-28 15:39:01 +0100

    a

--- app.py
+++ app.py
@@ -23,23 +23,28 @@ footer {visibility: hidden;}
 """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
 
-st.markdown("""<p>Il <b>problema delle regine</b> 👑 è un problema che consiste 
-nel trovare il modo di posizionare <i>N</i> donne (pezzo degli scacchi ♟) su 
+st.title("RISOLUZIONE DEL PROBLEMA DELLE REGINE UTILIZZANDO UN ALGORITMO GENETICO 🧬")
+
+st.markdown("""<hr><p>Il <b>problema delle regine</b> 👑 è un problema che consiste 
+nel trovare il modo di posizionare <i>N</i> <b>donne</b> (pezzo degli scacchi ♟) su 
 una scacchiera <i>N</i>x<i>N</i> tali che nessuna di esse possa catturarne un'altra, usando i 
 movimenti standard della regina. Perciò, una soluzione dovrà prevedere che 
-nessuna regina abbia una colonna, traversa o diagonale in comune con un'altra 
-regina.<br><br><b>Inserisci il numero di regine e scopri la soluzione</b></p>""", unsafe_allow_html=True)
+nessuna regina abbia una <b>colonna, traversa o diagonale</b> in comune con un'altra 
+regina.<br><br><i>La soluzione al problema viene trovata grazie ad un <b>algoritmo genetico</b> opportunamente progettato</i><br><br>
+<b>Inserisci il numero di regine e scopri la soluzione</b></p>""", unsafe_allow_html=True)
 
 
 regine = st.number_input('REGINE', min_value=4 ,step=1)
 
 
 zoom = (regine/regine**2) * 0.4
-a =st.button('Play')
 
+if regine > 12:
+    st.warning("Il tempo di esecuzione aumenta esponenzialmente all'aumentare del numero di regine")
+a =st.button('Play')
 if a is True:
 
-    start_time = time.time()
+    # start_time = time.time()
     soluzione = run_genetico(regine, 100)
 
     x = np.array(soluzione[0])-1
@@ -52,4 +57,4 @@ if a is True:
     ax.axes.xaxis.set_visible(False)
     ax.axes.yaxis.set_visible(False)
     st.pyplot()
-    st.write(time.time() - start_time)
+    # st.write(time.time() - start_time)
